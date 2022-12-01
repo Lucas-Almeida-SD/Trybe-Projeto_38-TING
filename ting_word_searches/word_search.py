3a5b1b3a4ef488b661ccb7cb08fcd7c2a70c9db0
lucas_almeida <lucas.almdiniz@gmail.com>
2022-12-01 17:37:16 -0300

    feat: Requisito 7: função "search_by_word" implementada
    verifica a existência de uma palavra em todos os arquivos processados e retorna uma lista de informações dos arquivos no qual a palavra foi encontrada, incluindo nessas informações o conteúdo das linhas

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,4 +1,4 @@
-def add_file_information_to_list(info_list, file, word):
+def add_file_information_to_list(info_list, file, word, include_content):
     info_file = {
         "palavra": word,
         "arquivo": file["nome_do_arquivo"],
@@ -7,23 +7,33 @@ def add_file_information_to_list(info_list, file, word):
 
     for row_index in range(file["qtd_linhas"]):
         row = file["linhas_do_arquivo"][row_index]
+        row_number = row_index + 1
 
-        if word.lower() in row.lower():
-            info_file["ocorrencias"].append({"linha":  row_index + 1})
+        if word.lower() in row.lower() and include_content:
+            info_file["ocorrencias"].append(
+                {"linha":  row_number, "conteudo": row}
+            )
+
+        elif word.lower() in row.lower():
+            info_file["ocorrencias"].append({"linha": row_number})
 
     if len(info_file["ocorrencias"]):
         info_list.append(info_file)
 
 
-def exists_word(word: str, instance):
+def create_info_list(word, instance, include_content):
     info_list = list()
 
     for file_index in range(len(instance)):
         file = instance.search(file_index)
-        add_file_information_to_list(info_list, file, word)
+        add_file_information_to_list(info_list, file, word, include_content)
 
     return info_list
 
 
+def exists_word(word, instance):
+    return create_info_list(word, instance, False)
+
+
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return create_info_list(word, instance, True)
