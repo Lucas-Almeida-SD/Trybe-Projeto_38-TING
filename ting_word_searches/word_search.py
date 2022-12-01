1bda9ec32be162464d9be22e18c9ecf0dc618f68
lucas_almeida <lucas.almdiniz@gmail.com>
2022-12-01 17:21:28 -0300

    feat: Requisito 6: função "exists_word" impementada
    verifica a existência de uma palavra em todos os arquivos processados e retorna uma lista de informações dos arquivos no qual a palavra foi encontrada

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+def add_file_information_to_list(info_list, file, word):
+    info_file = {
+        "palavra": word,
+        "arquivo": file["nome_do_arquivo"],
+        "ocorrencias": list()
+    }
+
+    for row_index in range(file["qtd_linhas"]):
+        row = file["linhas_do_arquivo"][row_index]
+
+        if word.lower() in row.lower():
+            info_file["ocorrencias"].append({"linha":  row_index + 1})
+
+    if len(info_file["ocorrencias"]):
+        info_list.append(info_file)
+
+
+def exists_word(word: str, instance):
+    info_list = list()
+
+    for file_index in range(len(instance)):
+        file = instance.search(file_index)
+        add_file_information_to_list(info_list, file, word)
+
+    return info_list
 
 
 def search_by_word(word, instance):
