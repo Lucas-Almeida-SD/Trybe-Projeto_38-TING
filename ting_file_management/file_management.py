f449b7e0d35b5fc8721ef4f87d6b876bf8d7c75f
lucas_almeida <lucas.almdiniz@gmail.com>
2022-12-01 15:05:32 -0300

    feat: Requisito 2: função "txt_importer" implementada
    realiza leitura do arquivo e retorna uma lista com o conteúdo

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    splited_path_file = path_file.split('.')
+    if splited_path_file[-1] != 'txt':
+        return sys.stderr.write("Formato inválido\n")
+
+    try:
+        with open(path_file, 'r') as file:
+            reader = file.read().split("\n")
+            return reader
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
